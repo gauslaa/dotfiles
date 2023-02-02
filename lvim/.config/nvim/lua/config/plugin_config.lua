8245c6da0a419dcf5be4d38f524914efa0d0d693
gauslaa <gauslaa@gauslaas-MacBook-Pro.local>
2023-02-02 20:40:31 +0100

    fix: change lead from ; to , for tex config

--- lvim/.config/nvim/lua/config/plugin_config.lua
+++ lvim/.config/nvim/lua/config/plugin_config.lua
@@ -1,6 +1,7 @@
 -- cahnge tectonic to the compiler of use.
 vim.g.vimtex_compiler_method = "tectonic"
--- syntax enable
+
+vim.cmd("syntax enable")
 
 vim.g.tex_flavor = "latex"
 
@@ -11,4 +12,4 @@ vim.g.vimtex_quickfix_mode = 0
 vim.opt.conceallevel = 1
 vim.g.tex_conceal = "abdmg"
 
-vim.g.maplocalleader = ";"
+vim.g.maplocalleader = ","
